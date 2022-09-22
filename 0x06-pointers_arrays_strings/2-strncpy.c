e2ea7cab5ae22845864f366908d6e8dff1f91725
rukayyamujittapha <you@example.com>
2022-09-22 09:09:10 -0700

    task 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	int i;
 
 	for (i = 0; i < n && *(src + i); i++)
 	{
@@ -23,5 +23,4 @@ int i;
 	}
 
 	return (dest);
-}
-      
+}      
